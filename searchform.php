a0063422d41dc22ac340b82cd9ce0564a2c5ccc3
Daniele Scasciafratte <mte90net@gmail.com>
2015-09-29 17:16:25 +0200

    working on panel

--- searchform.php
+++ searchform.php
@@ -1,10 +1,10 @@
 <form action="<?php echo get_site_url(); ?>" id="searchform" method="get" class="form-inline">
     <div class="input-group">
-	<input type="search" class="form-control" id="s" name="s" required />
-	<span class="input-group-btn">
-	    <button type="submit" class="btn btn-primary" id="searchsubmit" />
-	    <i class="fa fa-search"></i>
-	    </button>
-	</span>
+	<button type="submit" class="btn btn-info" id="searchsubmit" />
+	<i class="fa fa-search"></i>
+	</button>
+	<div class="form-group">
+	    <input type="search" class="form-control input-lg" id="s" name="s" required />
+	</div>
     </div>
 </form>
